fe1cb947fd9da5a1c4d0cc9980d2d6c41b2c47fb
Adam <ad-91@live.co.uk>
2021-03-17 22:54:55 +0000

    added note controller that works correctly

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('./connection');
 const express = require("express");
 const cors = require("cors");
 const {userRouter} = require("./routes/user");
+const {noteRouter} = require("./routes/note");
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(userRouter);
+app.use(noteRouter);
 
 app.get("/health", (req, res) => {
     res.status(200).send({message: "API is working!"})
