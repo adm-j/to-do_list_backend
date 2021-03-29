87d2b9159817c2fb6cb6f64ac9a65c67f92b71bd
Adam <ad-91@live.co.uk>
2021-03-29 19:16:53 +0100

    adjusted data sent to client to prevent sensitive data being sent back

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,10 +4,13 @@ const {User} = require("../models/User");
 exports.addUser = async (req, res) => {
     try {
         const user = new User(req.body);
-        const token = await user.generateAuthToken();
+        // const token = await user.generateAuthToken();
         const savedUser = await user.save();
+        const username = savedUser.username
+        console.log(username)
+        ///console.log(token)
 
-        res.status(200).send({savedUser, token});
+        res.status(200).send({message: "successfully created"});
 
     } catch (error) {
         if (error.code === 11000) {
@@ -16,7 +19,7 @@ exports.addUser = async (req, res) => {
         else {
         console.log(error);
         res.status(500).send({message: "Could not connect"});
-        }
+        } 
     }
 };
 
@@ -26,9 +29,12 @@ exports.login = async (req, res) => {
     try{
         const user = await User.findByCredentials(req.body.username, req.body.password);
         const token = user.generateAuthToken();
+        
+        const userName = user.username
+        const userToken = user.tokens[0]
 
         console.log("successful login");
-        res.status(200).send({user, token});
+        res.status(200).send({userName, userToken});
     } catch (error) {
         console.log(error);
         res.status(400).send({message: "unable to login"});
--- src/routes/note.js
+++ src/routes/note.js
@@ -3,7 +3,8 @@ const noteRouter = Router();
 
 const {addNote, deleteNote} = require("../controllers/note")
 
-noteRouter.post("/notes/:user_id", addNote) //fetch user notes
+noteRouter.post("/notes/:user_id", addNote)
 noteRouter.delete("/notes/:note_id", deleteNote)
+// noteRouter.get("/notes/:user_id", //showAllNotes)
 
 module.exports = {noteRouter};
